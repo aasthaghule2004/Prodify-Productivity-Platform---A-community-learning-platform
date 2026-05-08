72a8e187a4ea80d8716357d0f749bb7e4d46b7a0
aasthaghule2004 <aasthaghule4@gmail.com>
2026-05-08 15:40:37 +0530

    Serve uploaded PDFs on Render demo

--- prodify/urls.py
+++ prodify/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,3 +21,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Demo deployment media serving. For production, use persistent object storage.
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
